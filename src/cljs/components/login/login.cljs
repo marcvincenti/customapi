628a0102b73992b83f54463fabbd22ebdb983300
marcvincenti <vincentimarc@gmail.com>
2016-11-09 17:48:02 +0100

    patch infinite xhr loop

--- src/cljs/components/login/login.cljs
+++ src/cljs/components/login/login.cljs
@@ -4,35 +4,32 @@
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]))
 
-(def region-list (r/atom {}))
-
 (defn ^:private login-form []
-  (go (swap! region-list assoc :list (get-in (<! (http/get "/api/regions")) [:body :data])))
-
-  [:form {:class "form-horizontal"}
-  [:div {:class "form-group"}
-    [:label {:for "inputAccessKey" :class "col-sm-2 control-label"} "Access key"]
-    [:div {:class "col-sm-10"}
-      [:input {:type "text" :id "inputAccessKey" :class "form-control"
-               :placeholder "Access key" :required ""}]]]
-
-  [:div {:class "form-group"}
-    [:label {:for "inputSecretAccessKey" :class "col-sm-2 control-label"} "Secret access key"]
-    [:div {:class "col-sm-10"}
-      [:input {:type "password" :id "inputSecretAccessKey" :class "form-control"
-               :placeholder "Secret access key" :required ""}]]]
-
-  [:div {:class "form-group"}
-    [:label {:for "inputRegion" :class "col-sm-2 control-label"} "Region"]
-    [:div {:class "col-sm-10"}
-      [:select {:multiple "" :class "form-control" :id "inputRegion"}
-        (for [reg (:list @region-list)] ^{:key reg}
-          [:option {:value (:value reg)} (:name reg)])
-      ]]]
-
-  [:div {:class "form-group"}
-    [:div {:class "col-sm-offset-2 col-sm-10"}
-      [:button {:type "submit" :class "btn btn-default"} "Login"]]]])
+  (let [region-list (r/atom {})
+        get-regions (fn [] (go (swap! region-list assoc :list
+                      (get-in (<! (http/get "/api/regions")) [:body :data]))))]
+  (get-regions)
+  (fn []
+    [:form {:class "form-horizontal"}
+    [:div {:class "form-group"}
+      [:label {:for "inputAccessKey" :class "col-sm-2 control-label"} "Access key"]
+      [:div {:class "col-sm-10"}
+        [:input {:type "text" :id "inputAccessKey" :class "form-control"
+                 :placeholder "Access key" :required ""}]]]
+    [:div {:class "form-group"}
+      [:label {:for "inputSecretAccessKey" :class "col-sm-2 control-label"} "Secret access key"]
+      [:div {:class "col-sm-10"}
+        [:input {:type "password" :id "inputSecretAccessKey" :class "form-control"
+                 :placeholder "Secret access key" :required ""}]]]
+    [:div {:class "form-group"}
+      [:label {:for "inputRegion" :class "col-sm-2 control-label"} "Region"]
+      [:div {:class "col-sm-10"}
+        [:select {:multiple "" :class "form-control" :id "inputRegion"}
+          (for [reg (:list @region-list)] ^{:key reg}
+            [:option {:value (:value reg)} (:name reg)])]]]
+    [:div {:class "form-group"}
+      [:div {:class "col-sm-offset-2 col-sm-10"}
+        [:button {:type "submit" :class "btn btn-default"} "Login"]]]])))
 
 (defn component []
   [:div [:h1 "Login Page"]
